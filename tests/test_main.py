cd187f75cf74c91a44d75ad8226e3d0ab4fb1eca
Reuven V. Gonzales <reuven@tobetter.us>
2012-06-26 15:56:39 -0700

    Removed unnecessary code

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,39 +24,3 @@ def test_coordinator_start_and_stop(fake_thread_cls, fake_active):
     coordinator = ServerCoordinator.setup(fake_servers, fake_options, fake_config)
     coordinator.start()
     coordinator.stop()
-
-class TestServerCoordinator(object):
-    def setup(self):
-        # Fake Servers
-        fake_server1 = fudge.Fake('server1')
-        fake_server2 = fudge.Fake('server2')
-        fake_servers = [fake_server1, fake_server2]
-        fake_server1.provides('start_server')
-        fake_server2.provides('start_server')
-        # Fake Options
-        fake_options = fudge.Fake('options')
-
-        # Patch threading
-        patch_context = fudge.patch('threading.Thread', 'zmq.Context')
-        self.context = ContextUser(patch_context)
-        fake_thread_cls, fake_zmq_context = self.context.enter()
-        
-        # Fake threading.Thread Setup
-        fake_thread = fake_thread_cls.is_callable().returns_fake()
-        fake_thread.expects('start')
-
-        # Fake zmq.Context Setup
-        fake_socket = fake_zmq_context.is_callable().returns_fake()
-        fake_socket.is_a_stub()
-
-        self.fake_thread_cls = fake_thread_cls
-        self.fake_thread = fake_thread
-        self.fake_server1 = fake_server1
-        self.fake_server2 = fake_server2
-        self.fake_zmq_context = fake_zmq_context
-        self.coordinator = ServerCoordinator.setup(fake_servers, fake_options)
-
-    def teardown(self):
-        reset()
-        self.context.exit()
-
