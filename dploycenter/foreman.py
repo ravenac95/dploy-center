b41b03714994712bd34af76be3ce50d2cb524a6b
Reuven V. Gonzales <reuven@tobetter.us>
2012-06-27 16:56:22 -0700

    BuildForeman using builder pattern

--- dploycenter/foreman.py
+++ dploycenter/foreman.py
@@ -1,2 +1,15 @@
 class BuildForeman(object):
-    pass
+    """Coordinates packaging and distribution of cargo"""
+    def __init__(self, application, builder):
+        self.application = application
+        self.builder = builder
+
+    def prepare_cargo(self):
+        builder = self.builder 
+        
+        builder.prepare_container(self.application)
+        builder.build_application()
+        builder.compress_container()
+
+    def distribute_cargo(self):
+        cargo = self.builder.get_cargo()
--- tests/test_foreman.py
+++ tests/test_foreman.py
@@ -1,4 +1,37 @@
+import fudge
 from dploycenter.foreman import *
 
-def test_initialize_build_foreman():
-    foreman = BuildForeman()
+class TestBuildForeman(object):
+    def setup(self):
+        fake_builder = fudge.Fake('CargoBuilder')
+        fake_application = fudge.Fake('Application')
+        foreman = BuildForeman(fake_application, fake_builder)
+    
+        # Store fakes in class
+        self.fake_builder = fake_builder
+        self.fake_application = fake_application
+
+        # Store foreman in class
+        self.foreman = foreman
+    
+    @fudge.test
+    def test_prepare_cargo(self):
+        fake_builder = self.fake_builder
+
+        fake_builder.expects('prepare_container').with_args(self.fake_application)
+        fake_builder.expects('build_application')
+        fake_builder.expects('compress_container')
+
+        self.foreman.prepare_cargo()
+
+    @fudge.test
+    def test_distribute_cargo(self):
+        fake_builder = self.fake_builder
+
+        fake_cargo = fudge.Fake('Cargo')
+
+        fake_builder.expects('get_cargo').returns(fake_cargo)
+
+        self.foreman.distribute_cargo()
+
+
